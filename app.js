73ff6bad6dfef7e48d6b6e45a4c9765624483587
Akash22396 <akash.singh22396@gmail.com>
2022-09-11 11:05:57 +0530

    filterByObject function added

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-const { reduceArray } = require("./index");
+const { reduceArray, filterArray, filterByObject } = require("./index");
 
-// reduceArray(arrayData, operation = "sum", accum = 0,objectName);
 
-const sum = reduceArray(
-  [
-    { product: "pen", price: 5 },
-    { product: "pencil", price: 2 },
-  ],
-  "sum",
-  0,
-  "price"
-);
-// const sum = reduceArray([10, 20, 30], "sum", 0);
-// const minus = reduceArray([10, 20, -30, -3], "minus", 0);
-// const multiply = reduceArray([10, 20], "multiply", 1);
-// console.log(sum, minus, multiply);
-console.log(sum);
